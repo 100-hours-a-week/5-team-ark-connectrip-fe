387c1791dcbdd7128a48f6d89a921f356bff12e8
sen2y <pse6704@sookmyung.ac.kr>
2024-08-14 12:49:44 +0900

    💄아이콘 css update

--- src/app/components/Icon/CalendarIcon.tsx
+++ src/app/components/Icon/CalendarIcon.tsx
@@ -4,8 +4,8 @@ import React from 'react'
 const CalendarIcon: React.FC = () => {
   return (
     <svg
-      width='10'
-      height='11'
+      width='15'
+      height='16'
       viewBox='0 0 10 11'
       fill='none'
       xmlns='http://www.w3.org/2000/svg'
