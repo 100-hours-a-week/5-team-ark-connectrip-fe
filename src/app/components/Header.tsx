3832a742255857aa73bfb998d0f930859376ae2f
sen2y <pse6704@sookmyung.ac.kr>
2024-08-15 16:39:59 +0900

    ♻️ 페이지 주소 company ➜ accompany 로 변경

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -12,7 +12,7 @@ export default function Header() {
   const router = useRouter()
 
   const handleClick = () => {
-    router.push('/company')
+    router.push('/accompany')
   }
 
   const toggleDropdown = () => {
