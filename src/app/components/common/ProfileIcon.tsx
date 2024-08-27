83a1a5b55514c56dd0b2da8657ba4809a8dea0ca
sen2y <pse6704@sookmyung.ac.kr>
2024-08-27 12:52:47 +0900

    🐛 이미지 아이콘 타원형으로 표시되는 문제 해결 (#41)
    - Image 컴포넌트에 objectFit: 'cover' 스타일 추가
    - 이미지를 1:1 비율로 유지하며 둥글게 표시되도록 수정
    
    closes #41

--- src/app/components/common/ProfileIcon.tsx
+++ src/app/components/common/ProfileIcon.tsx
@@ -11,30 +11,39 @@ const ProfileIcon: React.FC<ProfileIconProps> = ({ src, size, nickname }) => {
   if (!src) {
     // 이미지가 없을 때
     return (
-      <div
-        className='flex items-center justify-center rounded-full'
-        style={{
-          width: size,
-          height: size,
-          backgroundColor: '#6d7992',
-          color: 'white',
-          fontSize: size * 0.4, // 아이콘 크기에 비례하여 글자 크기를 조정
-          fontWeight: 'bold',
-        }}
-      >
-        {nickname.charAt(0).toUpperCase()} {/* 닉네임의 첫 글자 */}
+      <div>
+        <div
+          className='flex items-center justify-center rounded-full'
+          style={{
+            width: size,
+            height: size,
+            backgroundColor: '#6d7992',
+            color: 'white',
+            fontSize: size * 0.4, // 아이콘 크기에 비례하여 글자 크기를 조정
+            fontWeight: 'bold',
+          }}
+        >
+          {nickname.charAt(0).toUpperCase()} {/* 닉네임의 첫 글자 */}
+        </div>
       </div>
     )
   }
 
   return (
-    <Image
-      src={src}
-      alt='Profile'
-      width={size}
-      height={size}
-      className='rounded-full'
-    />
+    <div>
+      <Image
+        src={src}
+        alt='Profile'
+        width={size}
+        height={size}
+        className='rounded-full'
+        style={{
+          objectFit: 'cover', // 이미지를 크기에 맞게 자름
+          width: size,
+          height: size,
+        }}
+      />
+    </div>
   )
 }
 
