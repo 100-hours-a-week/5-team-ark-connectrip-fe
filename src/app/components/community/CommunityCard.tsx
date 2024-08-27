0107b6f580f14b42caebe66c88fa72aaae3d99ae
sen2y <pse6704@sookmyung.ac.kr>
2024-08-28 00:51:40 +0900

    ✨ 커뮤니티 목록 페이지 구현

---
+++ src/app/components/community/CommunityCard.tsx
@@ -0,0 +1,43 @@
+'use client'
+
+import React, { useState, useEffect } from 'react'
+import ProfileIcon from '../common/ProfileIcon'
+import { truncateText } from '../../utils/textUtils'
+import { useTimeStamp } from '../../hooks/useTimeStamp'
+import { PostCardProps } from '@/interfaces'
+
+export default function CommunityCard({
+  title,
+  content,
+  createdAt,
+  nickname,
+  profileImagePath,
+}: PostCardProps) {
+  // useTimeStamp 커스텀 훅 사용
+  const timeAgo = useTimeStamp(createdAt)
+
+  return (
+    // <div className='bg-white p-4 rounded-lg shadow-md flex flex-col  mb-4 cursor-pointer '>
+    <div className='box-border bg-white border-solid border border-[#e7e4e4] hover:border-main  p-4 rounded-lg  flex flex-col  mb-4 cursor-pointer '>
+      <div className='flex items-center space-x-2 mb-1'>
+        <ProfileIcon
+          src={profileImagePath || ''}
+          size={30}
+          nickname={nickname}
+        />
+
+        <div className='text-sm font-semibold'>{nickname}</div>
+      </div>
+      <h2 className='text-lg font-semibold'>{truncateText(title, 20)}</h2>
+      <div className='flex justify-between items-end '>
+        <div className='text-sm text-gray-600 '>
+          {truncateText(content, 35)}
+        </div>
+
+        <div className='flex justify-between gap-2 text-sm text-gray-50 mt-1 text-s text-gray-500 whitespace-nowrap'>
+          {timeAgo}
+        </div>
+      </div>
+    </div>
+  )
+}
