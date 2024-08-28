a48327a84736dd3852d404f7936baaa1f467a994
sen2y <pse6704@sookmyung.ac.kr>
2024-08-28 13:12:49 +0900

    💄 화면 테두리 그림자 추가

--- src/app/components/common/ClientContent.tsx
+++ src/app/components/common/ClientContent.tsx
@@ -12,7 +12,7 @@ export default function ClientContent({
 }) {
   return (
     <div className='container mx-auto'>
-      <div className='flex flex-col h-screen'>
+      <div className='flex flex-col min-h-screen h-full shadow-lg'>
         <Header />
         <main className='flex flex-1 mt-[70px] justify-center items-start bg-white relative'>
           {children}
