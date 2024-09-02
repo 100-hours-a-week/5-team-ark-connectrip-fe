4ef6403bdf12242139db7b337d63053388f6fa77
sen2y <pse6704@sookmyung.ac.kr>
2024-09-02 23:05:15 +0900

    ⚡️ alert message 띄우는 시간 전역 설정

--- src/app/utils/alertUtils.ts
+++ src/app/utils/alertUtils.ts
@@ -4,25 +4,34 @@ import { message } from 'antd'
 export const useCustomMessage = () => {
   const [messageApi, contextHolder] = message.useMessage()
 
-  const showSuccess = (content: string) => {
+  const showSuccess = (content: string, callback?: () => void) => {
     messageApi.open({
       type: 'success',
       content,
     })
+    if (callback) {
+      setTimeout(callback, 1500) // 1.5초 대기 후 콜백 실행
+    }
   }
 
-  const showError = (content: string) => {
+  const showError = (content: string, callback?: () => void) => {
     messageApi.open({
       type: 'error',
       content,
     })
+    if (callback) {
+      setTimeout(callback, 1500) // 1.5초 대기 후 콜백 실행
+    }
   }
 
-  const showWarning = (content: string) => {
+  const showWarning = (content: string, callback?: () => void) => {
     messageApi.open({
       type: 'warning',
       content,
     })
+    if (callback) {
+      setTimeout(callback, 1500) // 1.5초 대기 후 콜백 실행
+    }
   }
 
   return { contextHolder, showSuccess, showError, showWarning }
