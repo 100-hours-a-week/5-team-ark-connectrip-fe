de5446706b083c26ce0a7ef61534733aa5635ff1
sen2y <pse6704@sookmyung.ac.kr>
2024-09-02 23:11:12 +0900

    ✨ 동행유저 mockdata 활용하여 동행인들의 위치 출력 로직

--- src/app/components/chat/GuestContent.tsx
+++ src/app/components/chat/GuestContent.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useMemo, useRef, useState } from 'react'
 import ProfileIcon from '../common/ProfileIcon'
 import { Button, Switch } from 'antd'
 import useAuthStore from '@/app/store/useAuthStore'
@@ -8,24 +8,76 @@ import { leaveChatRoom } from '@/app/utils/fetchUtils'
 import { useHandleDeleteClick } from '@/app/hooks/useHandleDeleteClick'
 import { useCustomMessage } from '@/app/utils/alertUtils'
 import { usePathname } from 'next/navigation'
-import { Map, MapMarker, CustomOverlayMap } from 'react-kakao-maps-sdk'
+import { Map, CustomOverlayMap, useMap } from 'react-kakao-maps-sdk'
 import useKakaoLoader from '@/app/hooks/useKakaoLoader'
+import LoadingSpinner from '../common/LoadingSpinner'
+
+const mockData = [
+  {
+    memberId: 1,
+    nickname: '트룰루',
+    profileImage:
+      'http://k.kakaocdn.net/dn/ZEQvV/btsIzAcVyYL/TkOaaPbnv0jGFEq3idt6Ck/img_640x640.jpg',
+    LastLocation: {
+      latitude: 37.5665,
+      longitude: 126.978,
+    },
+  },
+  {
+    memberId: 2,
+    nickname: '션말고숀',
+    profileImage:
+      'http://k.kakaocdn.net/dn/wAv8y/btsJg9GuEd2/F1WBAuNghQpR8u4dMM5Qn1/img_640x640.jpg',
+    LastLocation: {
+      latitude: 35.1796,
+      longitude: 129.0756,
+    },
+  },
+  {
+    memberId: 3,
+    nickname: '냄뀨뀨',
+    profileImage: ' ',
+    LastLocation: {
+      latitude: 37.4563,
+      longitude: 126.7052,
+    },
+  },
+  {
+    memberId: 4,
+    nickname: '토파즈',
+    profileImage:
+      'http://k.kakaocdn.net/dn/cr7joo/btsI7OuRmrZ/wm49uKvLgMskVh5ZWWdCVk/m1.jpg',
+    LastLocation: {
+      latitude: 35.8722,
+      longitude: 128.6025,
+    },
+  },
+  {
+    memberId: 5,
+    nickname: '노노아',
+    profileImage: '',
+    LastLocation: {
+      latitude: 37.5113,
+      longitude: 127.098,
+    },
+  },
+]
 
 interface GuestContent {
   companionUsers: CompanionUsers[] // 동행 참여자 목록
   postId: number // 게시글 ID
 }
 
-const HostContent: React.FC<GuestContent> = ({ companionUsers, postId }) => {
-  const { nickname } = useAuthStore() // zustand 스토어에서 유저 닉네임 가져오기
+const GuestContent: React.FC<GuestContent> = ({ companionUsers, postId }) => {
+  const { nickname, profileImage } = useAuthStore() // zustand 스토어에서 유저 닉네임 가져오기
   const [trackingEnabled, setTrackingEnabled] = useState(false)
-  // TODO : 내 위치 전송 기능 채팅과 연결 후 주석 삭제
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [locationLink, setLocationLink] = useState<string | null>(null)
-  const [location, setLocation] = useState<{ lat: number; lng: number }>({
-    lat: 33.450701,
-    lng: 126.570667,
-  })
+  const [location, setLocation] = useState<{
+    latitude: number
+    longitude: number
+  }>()
+  const mapRef = useRef<kakao.maps.Map | null>(null) // 지도 참조를 위해 useRef 사용
+  const [loading, setLoading] = useState(false) // 로딩 상태 추가
   const router = useRouter()
   const handleDeleteClick = useHandleDeleteClick() // 모달 호출 유틸리티 사용
   const { contextHolder, showSuccess, showWarning, showError } =
@@ -38,32 +90,51 @@ const HostContent: React.FC<GuestContent> = ({ companionUsers, postId }) => {
   // 내 위치 추적 스위치 변경 핸들러
   const handleSwitchChange = (checked: boolean) => {
     setTrackingEnabled(checked)
+    if (checked) {
+      setLoading(true)
+      fetchLocation()
+    }
   }
 
-  //  내 위치 전송 핸들러
+  // 내 위치 전송 버튼 클릭 핸들러
   const handleSendLocation = () => {
     if (!navigator.geolocation) {
       showError('위치 정보를 가져올 수 없습니다.')
+      return
+    } else if (!trackingEnabled) {
+      showError('내 위치 추적을 허용해주세요.')
+    }
+
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const mapNickname = nickname || '사용자'
+        const { latitude, longitude } = position.coords
+        const kakaoMapLink = `https://map.kakao.com/link/map/${mapNickname}님의_현재위치,${latitude},${longitude}`
+        setLocationLink(kakaoMapLink)
+        console.log(kakaoMapLink)
+      },
+      (error) => {
+        console.error('Error fetching location:', error)
+        showError('팝업에서 위치 정보를 허용해주세요.')
+      }
+    )
+  }
+
+  const fetchLocation = () => {
+    if (!navigator.geolocation) {
+      showError('위치 정보를 가져올 수 없습니다.')
+      setLoading(false) // 로딩 종료
       return
     }
 
     navigator.geolocation.getCurrentPosition(
       (position) => {
         const { latitude, longitude } = position.coords
-        if (trackingEnabled) {
-          const mapNickname = nickname || '사용자'
-          const kakaoMapLink = `https://map.kakao.com/link/map/${mapNickname}님의_현재위치,${latitude},${longitude}`
-          setLocationLink(kakaoMapLink)
-          setLocation({ lat: latitude, lng: longitude }) // 위치 저장
-          // TODO : 채팅방에 링크 전송 기능 채팅과 연결 후 주석 삭제
-          console.log(kakaoMapLink)
-        } else {
-          showError('위치 추적이 비활성화되어 있습니다.')
-          console.log('Location sharing is disabled.')
-        }
+        setLocation({ latitude: latitude, longitude: longitude }) // 위치 저장
+        setLoading(false) // 로딩 종료
       },
       (error) => {
-        console.error('Error fetching location:', error)
+        console.error('Error fetching location: ', error)
         showError('팝업에서 위치 정보를 허용해주세요.')
       }
     )
@@ -79,40 +150,132 @@ const HostContent: React.FC<GuestContent> = ({ companionUsers, postId }) => {
     }
   }
 
+  const allLocations = [
+    ...mockData.map((user) => user.LastLocation),
+    location,
+  ].filter(
+    (loc): loc is { latitude: number; longitude: number } => loc !== undefined
+  ) // 필터링하여 undefined 제거
+
+  const ReSetttingMapBounds = ({
+    points,
+  }: {
+    points: { latitude: number; longitude: number }[]
+  }) => {
+    const map = useMap()
+
+    const bounds = useMemo(() => {
+      const bounds = new kakao.maps.LatLngBounds()
+
+      points.forEach((point) => {
+        bounds.extend(new kakao.maps.LatLng(point.latitude, point.longitude))
+      })
+
+      return bounds
+    }, [points])
+
+    useEffect(() => {
+      if (map && points.length > 0) {
+        map.setBounds(bounds) // 지도 범위를 설정
+      }
+    }, [map, bounds, points])
+
+    return null // 렌더링할 내용 없음
+  }
+
+  useEffect(() => {
+    if (trackingEnabled && allLocations.length > 0) {
+      updateMapBounds()
+    }
+  }, [trackingEnabled, location])
+
+  const updateMapBounds = () => {
+    if (mapRef.current && allLocations.length > 0) {
+      const bounds = new kakao.maps.LatLngBounds()
+
+      allLocations.forEach((loc) => {
+        if (loc) {
+          const point = new kakao.maps.LatLng(loc.latitude, loc.longitude)
+          bounds.extend(point)
+        }
+      })
+
+      mapRef.current.setBounds(bounds)
+    }
+  }
+
   return (
     <div className='flex flex-col gap-3 mb-3'>
       {contextHolder}
       {/* 동행 위치 조회 지도 컴포넌트 */}
-      <div className='flex justify-center items-center h-[300px] bg-gray-200'>
-        <Map
-          id='map'
-          center={location} // 저장된 위치 사용
-          style={{
-            width: '100%',
-            height: '300px', // 높이 조정
-          }}
-          level={3}
-        >
-          {/* 마커 생성 */}
-          <MapMarker
-            position={location}
-            image={{
-              src: 'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_red.png', // 마커이미지의 주소입니다
-              size: {
-                // 마커이미지의 크기
-                width: 64,
-                height: 69,
-              },
-              // 마커이미지의 옵션. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정
-              options: {
-                offset: {
-                  x: 27,
-                  y: 69,
-                },
-              },
+      <div className='flex justify-center items-center h-[300px] bg-gray-100'>
+        {loading ? (
+          <LoadingSpinner /> // 로딩 중일 때 스피너 표시
+        ) : trackingEnabled && location ? (
+          <Map
+            id='map'
+            center={{
+              // 지도의 중심좌표
+              lat: 33.450701,
+              lng: 126.570667,
             }}
-          />
-        </Map>
+            style={{
+              // 지도의 크기
+              width: '100%',
+              height: '300px',
+            }}
+            level={3} // 지도의 확대 레벨
+            onCreate={(map) => (mapRef.current = map)}
+          >
+            {/* 사용자 위치 커스텀 오버레이 */}
+            <CustomOverlayMap
+              position={{ lat: location.latitude, lng: location.longitude }}
+            >
+              <div className='-top-10 -left-5 w-[40px] h-[40px] rounded-full overflow-hidden bg-main border-main border-2 z-10'>
+                <ProfileIcon
+                  src={profileImage || ''}
+                  size={35}
+                  nickname={nickname || ''}
+                />
+                {/* 핀 모양 삼각형 */}
+                <div className='absolute bottom-[-7px] left-1/2 transform -translatitudee-x-1/2 w-0 h-0 border-l-[6px] border-l-transparent border-r-[6px] border-r-transparent border-t-[10px] border-t-main z-0' />
+              </div>
+            </CustomOverlayMap>
+
+            {/* 동행자들 위치 커스텀 오버레이 */}
+            {mockData.map(
+              (user) =>
+                user.LastLocation && (
+                  <CustomOverlayMap
+                    position={{
+                      lat: user.LastLocation.latitude,
+                      lng: user.LastLocation.longitude,
+                    }}
+                    key={user.memberId}
+                  >
+                    <div className='-top-10 -left-5 w-[40px] h-[40px] rounded-full overflow-hidden bg-main border-main border-2 z-10'>
+                      <ProfileIcon
+                        src={user.profileImage || ''}
+                        size={35}
+                        nickname={user.nickname}
+                      />
+                      {/* 핀 모양 삼각형 */}
+                      <div className='absolute bottom-[-7px] left-1/2 transform -translatitudee-x-1/2 w-0 h-0 border-l-[6px] border-l-transparent border-r-[6px] border-r-transparent border-t-[10px] border-t-main z-0' />
+                    </div>
+                  </CustomOverlayMap>
+                )
+            )}
+            {allLocations.length > 0 && (
+              <ReSetttingMapBounds points={allLocations} />
+            )}
+          </Map>
+        ) : (
+          <p className='text-center'>
+            내 위치 추적을 허용하면
+            <br />
+            동행들의 위치를 확인할 수 있어요!
+          </p>
+        )}
       </div>
       <div className='flex justify-between items-center'>
         <Button className='w-auto rounded-full' onClick={handleSendLocation}>
@@ -157,4 +320,4 @@ const HostContent: React.FC<GuestContent> = ({ companionUsers, postId }) => {
   )
 }
 
-export default HostContent
+export default GuestContent
