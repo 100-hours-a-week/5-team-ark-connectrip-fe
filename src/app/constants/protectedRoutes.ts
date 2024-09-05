0939c02a972802217bf39d52a8cc73118899352a
sen2y <pse6704@sookmyung.ac.kr>
2024-09-05 20:00:36 +0900

    ✨ 약관, 개인정보페이지 접근권한 제어

--- src/app/constants/protectedRoutes.ts
+++ src/app/constants/protectedRoutes.ts
@@ -2,7 +2,7 @@
 export const protectedRoutes = ['/accompany', '/chat', '/community', '/profile']
 
 // 추가 회원가입이 필요한 페이지 목록
-export const signupOnlyRoutes = ['/signup']
+export const signupOnlyRoutes = ['/signup', '/privacy', '/policy']
 
 // 로그인이 되면 접근할 수 없는 페이지 목록
 export const guestRoutes = ['/', 'signup']
