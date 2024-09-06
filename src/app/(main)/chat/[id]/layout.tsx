9733c62fb972034d0dd8e8b877d4c234e2561aa7
sen2y <pse6704@sookmyung.ac.kr>
2024-09-06 17:59:04 +0900

    🐛 빌드에러 요소 제거

--- src/app/(main)/chat/[id]/layout.tsx
+++ src/app/(main)/chat/[id]/layout.tsx
@@ -3,13 +3,14 @@
 import React, { Suspense } from 'react'
 import LoadingSpinner from '@/app/components/common/LoadingSpinner'
 import Script from 'next/script'
-export const API = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAOJSKEY}&libraries=services,clusterer&autoload=false`
 
 export default function AccompanyLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+  const API = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAOJSKEY}&libraries=services,clusterer&autoload=false`
+
   return (
     <div className='w-full h-full flex justify-center items-center'>
       <div className='w-full max-w-[500px] h-full'>
