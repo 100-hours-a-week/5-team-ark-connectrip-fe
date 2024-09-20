3bd218d3d9bea497d5fc17ab6ffb0b7b2bcdf7bf
sen2y <pse6704@sookmyung.ac.kr>
2024-09-20 22:22:48 +0900

    ✨ 프로필 조회 api 연동

--- src/app/(main)/(auth)/profile/[id]/page.tsx
+++ src/app/(main)/(auth)/profile/[id]/page.tsx
@@ -3,90 +3,87 @@ import ProfileCard from '@/app/components/profile/ProfileCard'
 import Introduction from '@/app/components/profile/Introduction'
 import Reviews from '@/app/components/profile/Reviews'
 import { Button } from 'antd'
-import { useRouter } from 'next/navigation'
+import { usePathname, useRouter } from 'next/navigation'
+import { useEffect, useState } from 'react'
+import { fetchUserProfile } from '@/app/utils/fetchUtils'
+import LoadingSpinner from '@/app/components/common/LoadingSpinner'
+import { ProfileData } from '@/interfaces'
 import { LeftOutlined } from '@ant-design/icons'
-
-const mockData = {
-  memberId: 2,
-  profileImagePath:
-    'http://k.kakaocdn.net/dn/rzia4/btsEYPys3Ja/IIpIg8cU6mzrZm2DuNq5SK/img_640x640.jpg',
-  nickname: '센세니',
-  gender: 'W',
-  accompanyCount: 0,
-  reviewCount: 5,
-  recentReviews: [
-    {
-      reviewId: 9,
-      content: '성격도 너무 잘맞고 즐거운 여행이었어요! 다음에 또 같이 가요~',
-      reviewerNickname: '트룰루',
-      reviewerProfile: null,
-      reviewerId: 7,
-      createdAt: '2024-09-16T12:02:51Z',
-    },
-    {
-      reviewId: 8,
-      content:
-        '즐거운여행이었습니다. 즐거운여행이었습니다. 즐거운여행이었습니다. 즐거운여행이었습니다. 즐거운여행이었습니다. 즐거운여행이었습니다. 즐거운여행이었습니다. 즐거운여행이었습니다. 즐거운여행이었습니다. 즐거운여행이었습니다.',
-      reviewerNickname: '뿡박사님을 아세요',
-      reviewerProfile: null,
-      reviewerId: 6,
-      createdAt: '2024-09-11T04:02:35Z',
-    },
-    {
-      reviewId: 7,
-      content: '즐거웠고, 다신 보지 말자',
-      reviewerNickname: '제푸푸',
-      reviewerProfile: null,
-      reviewerId: 5,
-      createdAt: '2024-09-11T04:02:21Z',
-    },
-  ],
-  description:
-    '트룰루입니다.트룰루입니다트룰루입니다트룰루입니다트룰루입니다트룰루입니다트룰루입니다트룰루입니다트룰루입니다트룰루입니다트룰루입니다트룰루입니다.',
-  ageGroup: '20대',
-}
 
 export default function ProfilePage() {
   const router = useRouter()
+  const [profileData, setProfileData] = useState<ProfileData | null>(null)
+  const path = usePathname()
+  const userId = path.split('/').pop()
+
+  console.log(userId)
+
+  useEffect(() => {
+    const loadProfileData = async () => {
+      try {
+        if (!userId) return
+        const data = await fetchUserProfile(parseInt(userId))
+        setProfileData(data)
+      } catch (error) {
+        console.error('프로필 데이터를 불러오는 중 오류가 발생했습니다:', error)
+      }
+    }
+
+    loadProfileData()
+  }, [userId])
 
   const handleNavigateToReviews = () => {
-    // router.push('/profile/reviews/${id}')
+    router.push('/profile/reviews')
   }
+
+  const handleEditToProfile = () => {
+    router.push('/profile/edit')
+  }
+
+  if (!profileData) {
+    return <LoadingSpinner />
+  }
+
   return (
-    <div className='w-full h-full p-4'>
-      <div className='flex justify-between pr-1 text-right text-base mb-2'>
-        <div
-          onClick={() => router.back()}
-          className='text-sm cursor-pointer text-secondary hover:text-black'
-        >
-          <LeftOutlined style={{ fontSize: 16 }} />
-        </div>
-        <p className='underline underline-offset-2 hover:text-gray-600 cursor-pointer'>
-          수정하기
-        </p>
+    <div className='w-full h-full p-4 overflow-auto mb-12'>
+      <div
+        onClick={() => router.back()}
+        className='text-sm cursor-pointer text-secondary hover:text-black pl-1 text-left mb-2'
+      >
+        <LeftOutlined style={{ fontSize: 16 }} />
       </div>
+
       <ProfileCard
-        profileImage={mockData.profileImagePath}
-        nickname={mockData.nickname}
-        accompanyCount={mockData.accompanyCount}
-        reviewCount={mockData.reviewCount}
+        profileImage={profileData.profileImagePath}
+        nickname={profileData.nickname}
+        accompanyCount={profileData.accompanyCount || 0} // Provide a default value of 0
+        reviewCount={profileData.reviewCount || 0} // Provide a default value of 0
       />
+
       <Introduction
-        ageGroup={mockData.ageGroup}
-        gender={mockData.gender}
-        description={mockData.description}
+        ageGroup={profileData.ageGroup}
+        gender={profileData.gender || ''}
+        description={profileData.description}
       />
       <div className='flex flex-col gap-3 mt-5'>
-        <h3 className='p-1 text-lg font-bold'>{mockData.nickname} 님의 후기</h3>
-        <Reviews reviews={mockData.recentReviews} />
+        <h3 className='p-1 text-lg font-bold'>
+          {profileData.nickname} 님의 후기
+        </h3>
+        <Reviews reviews={profileData.recentReviews} />
       </div>
-      <Button
-        type='primary'
-        className='w-full mt-4'
-        onClick={handleNavigateToReviews}
-      >
-        후기 더 표시하기
-      </Button>
+      {profileData.recentReviews.length === 0 ? (
+        <div className='text-center text-base text-gray-500 mt-10'>
+          작성된 후기가 없습니다.
+        </div>
+      ) : (
+        <Button
+          type='primary'
+          className='w-full mt-4 mb-4'
+          onClick={handleNavigateToReviews}
+        >
+          후기 더 표시하기
+        </Button>
+      )}
     </div>
   )
 }
