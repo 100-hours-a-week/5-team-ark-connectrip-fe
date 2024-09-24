9e7faea0cb5d03d9b93fc7981f94c360452b4d22
sen2y <pse6704@sookmyung.ac.kr>
2024-09-24 17:19:09 +0900

    ⚡️ 이미지 로딩속도 개선

--- src/app/components/accompany/CarouselComponent.tsx
+++ src/app/components/accompany/CarouselComponent.tsx
@@ -1,22 +1,42 @@
 import React from 'react'
 import { Carousel } from 'antd'
+import Image from 'next/image' // next/image 임포트
 
 const CarouselComponent: React.FC = () => {
   // 이미지 데이터를 배열로 정의
   const bannerImages = [
-    { src: '/banner/connectrip.svg', alt: '우리 동행의 시작, 커넥트립' },
-    { src: '/banner/yangyang.svg', alt: '양양 여행 이미지' },
-    { src: '/banner/jeju.svg', alt: '제주 여행 이미지' },
+    {
+      src: '/connectrip.png',
+      alt: '커넥트립 이미지',
+      width: 1920,
+      height: 1080,
+    },
+    {
+      src: '/yangyang.png',
+      alt: '양양 여행 이미지',
+      width: 1920,
+      height: 1080,
+    },
+    {
+      src: '/jeju.png',
+      alt: '제주 여행 이미지',
+      width: 1920,
+      height: 1080,
+    },
   ]
 
   return (
     <Carousel autoplay autoplaySpeed={4000} draggable>
       {bannerImages.map((image, index) => (
         <div key={index} className='relative w-full rounded-xl'>
-          <img
+          <Image
             src={image.src}
             alt={image.alt}
+            width={image.width}
+            height={image.height}
             className='w-full object-cover rounded-xl'
+            priority={index === 0}
+            layout='responsive'
           />
         </div>
       ))}
