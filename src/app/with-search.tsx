0b392299a276f8b005a688f26bc64870f797f41e
sen2y <pse6704@sookmyung.ac.kr>
2024-09-30 22:05:44 +0900

    ⚡️ 인트로페이지 렌더링 시간 개선 시도 - png-> svg

--- src/app/with-search.tsx
+++ src/app/with-search.tsx
@@ -48,13 +48,6 @@ export default function Home() {
   return (
     <div className='relative w-full h-screen flex justify-center items-center px-2.5'>
       {contextHolder}
-      {/* <Image
-        src={`/intro/${currentImage === 6 ? 1 : currentImage}.png`}
-        alt={`Intro ${currentImage}`}
-        fill // Next.js 13 이상에서는 fill 속성 사용
-        style={{ objectFit: 'cover', zIndex: 31 }} // 이미지 비율 유지하며 컨테이너에 맞춤
-        priority={true} // 이미지 로딩 우선순위 설정
-      /> */}
       <Image
         src={`/intro/background.png`}
         alt={`Intro`}
@@ -62,25 +55,17 @@ export default function Home() {
         style={{ objectFit: 'cover', zIndex: 31 }} // 이미지 비율 유지하며 컨테이너에 맞춤
         priority={true} // 이미지 로딩 우선순위 설정
       />
-      {currentImage === 1 && (
-        <div>
-          <img
-            src='/intro/1.svg'
-            alt='Intro 1'
-            className='absolute top-[5%] left-[10%] w-4/5 h-4/5 z-[32]'
-          />
-        </div>
-      )}
+
       {currentImage === 2 && (
         <div>
           <img
             src='/intro/2-3.svg'
-            alt='Intro 2'
+            alt='Intro 2-3'
             className='absolute top-[-10%] left-[10%] w-4/5 h-4/5 z-[32]'
           />
           <img
             src='/intro/2-2.svg'
-            alt='Intro 2-1'
+            alt='Intro 2-2'
             className='absolute top-[15%] right-[10%] w-4/5 h-4/5 z-[32]'
           />
         </div>
@@ -90,12 +75,12 @@ export default function Home() {
         <div>
           <img
             src='/intro/3-1.svg'
-            alt='Intro 3'
+            alt='Intro 3-1'
             className='absolute top-[-10%] left-[10%] w-4/5 h-4/5 z-[32]'
           />
           <img
             src='/intro/3-2.svg'
-            alt='Intro 3-1'
+            alt='Intro 3-2'
             className='absolute top-[17%] right-[8%] w-4/5 h-4/5 z-[32]'
           />
         </div>
@@ -105,17 +90,17 @@ export default function Home() {
         <div>
           <img
             src='/intro/4-1.svg'
-            alt='Intro 4'
+            alt='Intro 4-1'
             className='absolute top-[-12%] left-[19%] w-2/5 h-4/5 z-[32]'
           />
           <img
             src='/intro/4-2.svg'
-            alt='Intro 4-1'
+            alt='Intro 4-2'
             className='absolute top-[-9%] right-[0%] w-full h-4/5 z-[32]'
           />
           <img
             src='/intro/4-3.svg'
-            alt='Intro 4-2'
+            alt='Intro 4-3'
             className='absolute top-[19%] right-[-4%] w-[92%] h-4/5 z-[32]'
           />
         </div>
@@ -142,20 +127,29 @@ export default function Home() {
       )}
       {/* 카카오 로그인 버튼 (currentImage가 1 또는 6일 때 표시) */}
       {(currentImage === 1 || currentImage === 6) && (
-        <button
-          type='button'
-          onClick={kakaoLoginHandler}
-          className='absolute w-4/5 flex items-center gap-1.5 h-11 bg-yellow-400 text-[14.5px] font-medium justify-center z-40 rounded-xl absolute top-[60%]'
-        >
-          <Image
-            src='/kakao.svg'
-            alt='카카오 로그인'
-            width={20}
-            height={20}
-            priority={true}
-          />
-          <div>카카오 로그인</div>
-        </button>
+        <>
+          <div>
+            <img
+              src='/intro/1.svg'
+              alt='Intro 1'
+              className='absolute top-[5%] left-[10%] w-4/5 h-4/5 z-[32]'
+            />
+          </div>
+          <button
+            type='button'
+            onClick={kakaoLoginHandler}
+            className='absolute w-4/5 flex items-center gap-1.5 h-11 bg-yellow-400 text-[14.5px] font-medium justify-center z-40 rounded-xl absolute top-[60%]'
+          >
+            <Image
+              src='/kakao.svg'
+              alt='카카오 로그인'
+              width={20}
+              height={20}
+              priority={true}
+            />
+            <div>카카오 로그인</div>
+          </button>
+        </>
       )}
 
       {/* 스킵하기 버튼 (currentImage가 2~5일 때 표시) */}
